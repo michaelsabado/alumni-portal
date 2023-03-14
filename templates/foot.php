b7f8c1994a84fc1e7830fcde0083fb9352913c0f
Michael Sabado <81413787+michaelsabado@users.noreply.github.com>
2023-03-14 21:16:22 +0800

    Update foot.php

--- templates/foot.php
+++ templates/foot.php
@@ -2,6 +2,6 @@
     <span class="close">&times;</span>
     <img class="modal-contents mb-5" id="full-image">
 </div>
-<div id="foot" class="text-center">
+<div id="foot" class="text-center bg-info">
     <div class="h6 mb-0 text-white">&copy; Copyright 2022 - POLYTECHNIC COLLEGE OF LA UNION</div>
 </div>
