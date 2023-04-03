5b6c53ccdb6536614f6579f78d19f9b498655e98
Michael Sabado <81413787+michaelsabado@users.noreply.github.com>
2023-04-03 21:42:52 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -25,29 +25,29 @@ function setData($a, $b, $c, $d, $e = null, $app = null)
         // Server settings
         // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-        $mail->isSMTP();
-        $mail->Host = 'sandbox.smtp.mailtrap.io';
-        $mail->SMTPAuth = true;
-        $mail->Port = 2525;
-        $mail->Username = '7bbcdd4a9cb05c';
-        $mail->Password = '71277b41b355a5';
-
         // $mail->isSMTP();
-        // $mail->Host = 'smtp.gmail.com';
+        // $mail->Host = 'sandbox.smtp.mailtrap.io';
         // $mail->SMTPAuth = true;
-        // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        // $mail->Port = 587;
+        // $mail->Port = 2525;
+        // $mail->Username = '7bbcdd4a9cb05c';
+        // $mail->Password = '71277b41b355a5';
 
-        // $mail->Username = 'alumniportalpclu@gmail.com'; // YOUR gmail email
-        // $mail->Password = 'nwdgjkpisuoxfvtd'; // YOUR gmail password
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->Username = 'almatechpclu@gmail.com'; // YOUR gmail email
+        $mail->Password = 'pfiyetbtxhunsyph'; // YOUR gmail password
 
         // Sender and recipient settings
-        $mail->setFrom('alumniportalpclu@gmail.com', 'PCLU Alumni Portal');
+        $mail->setFrom('almatechpclu@gmail.com', 'PCLU Alumni Portal');
         $mail->addAddress($toEmail, $toName);
         if ($e !== null) {
             $mail->addReplyTo($app, $app); // to set the reply to
         } else {
-            $mail->addReplyTo('alumniportalpclu@gmail.com', 'PCLU Alumni Portal'); // to set the reply to
+            $mail->addReplyTo('almatechpclu@gmail.com', 'PCLU Alumni Portal'); // to set the reply to
         }
 
 
