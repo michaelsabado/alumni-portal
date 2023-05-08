9a09c511c9ff72278078db2f61711d750eef060c
Michael Sabado <81413787+michaelsabado@users.noreply.github.com>
2023-05-09 01:41:52 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -438,7 +438,7 @@ if (isset($_GET['filter'])) {
                             display: false
                         }]
                     },
-                    legend: false
+                    // legend: true
                 }
             });
         });
